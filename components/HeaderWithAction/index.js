1a6e6bddfe0885a611f225a9a14767529d1029a6
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-17 02:55:36 +0300

    Create HeaderWithAction component

---
+++ components/HeaderWithAction/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Icon } from "react-native-elements";
+
+export default class extends React.Component {
+  render() {
+    const { container, header } = styles;
+    const { title } = this.props;
+    return (
+      <View style={[styles.container]}>
+        <Text color="#ffffff" style={[styles.header]}>
+          {title}
+        </Text>
+        <Icon
+          raised
+          name="plus"
+          type="entypo"
+          color="#fefefd"
+          size={30}
+          iconStyle={{ margin: 0, padding: 0 }}
+          containerStyle={{
+            backgroundColor: "transparent",
+            margin: 0,
+            padding: 0
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 20
+  },
+  header: {
+    fontSize: 32,
+    color: "#ffffff",
+    textShadowColor: "rgba(0,0,0,0.25)",
+    textShadowRadius: 2,
+    textShadowOffset: { width: 0, height: 3 }
+  }
+});
