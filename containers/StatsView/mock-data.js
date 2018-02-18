34dcfd567c1b480368b78fd4c17ed16f04294c23
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-18 23:11:20 +0300

    Move mock data to separate file

---
+++ containers/StatsView/mock-data.js
@@ -0,0 +1,91 @@
+export const items = [
+	{
+		day: 27,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 28,
+		spending: '1500',
+		cashFlow: [
+			{
+				name: 'PlayStation',
+				change: 'positive',
+				value: '+$515',
+				time: '04:19 PM',
+				date: '31 Aug 2017'
+			},
+			{
+				name: 'PlayStation',
+				change: 'negative',
+				value: '+$515',
+				time: '04:19 PM',
+				date: '31 Aug 2017'
+			},
+			{
+				name: 'PlayStation',
+				change: 'positive',
+				value: '+$515',
+				time: '04:19 PM',
+				date: '31 Aug 2017'
+			},
+			{
+				name: 'PlayStation',
+				change: 'positive',
+				value: '+$515',
+				time: '04:19 PM',
+				date: '31 Aug 2017'
+			}
+		],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 29,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 30,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 31,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 1,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 2,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 3,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 4,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	},
+	{
+		day: 5,
+		spending: '1500',
+		cashFlow: [],
+		fillHeight: `${Math.random() * 70}`
+	}
+];
