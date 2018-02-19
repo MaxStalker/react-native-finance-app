66a3b7875484d28e0b5aecead26c52f1f2fa88b3
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-19 12:48:56 +0300

    Adjust TimelineChain component with new colors and layout

--- components/TimelineChain/index.js
+++ components/TimelineChain/index.js
@@ -1,43 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Column } from '../Common';
-import { LIGHT_GREY, RED } from '../../theme/colors';
+import { DARK_GREY } from '../../theme/colors';
 
 export const ChainContainer = Column.extend`
-	width: 60px;
-	height: 50px;
+	padding-left: 30px;
+	padding-right: 15px;
 	align-items: center;
+	justify-content: center;
 `;
 
 export const Line = styled.View`
 	width: 2px;
-	flex: 1;
-	background-color: ${LIGHT_GREY};
+	background-color: ${({ index, first }) =>
+		index === 0 && first ? 'rgba(0,0,0,0)' : DARK_GREY};
+	flex: ${({ flexValue }) => (flexValue ? flexValue : 1)}};
 `;
 
 export const CircleContainer = styled.View`
-	width: 12px;
-	height: 12px;
+	width: 14px;
+	height: 14px;
 	border-radius: 20px;
 	border-width: 2px;
-	border-color: ${LIGHT_GREY};
+	border-color: ${DARK_GREY};
 	background-color: transparent;
 `;
 
 export const CircleInset = styled.View`
 	position: absolute;
-	width: 10px;
-	height: 10px;
+	width: 6px;
+	height: 6px;
 	border-radius: 10px;
-	top: 3px;
-	left: 3px;
-	background-color: ${LIGHT_GREY};
+	top: 2px;
+	left: 2px;
+	background-color: ${DARK_GREY};
 `;
 
-export default () => {
+export default ({ number }) => {
 	return (
 		<ChainContainer>
-			<Line />
+			<Line flexValue={0.5} index={number} first />
 			<CircleContainer>
 				<CircleInset />
 			</CircleContainer>
