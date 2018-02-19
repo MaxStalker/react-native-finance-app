4d456ca4e26c3bcf36f20d352ce511d07031bb0d
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-19 12:50:00 +0300

    Adjust styling in SpendingList

--- components/SpendingList/styles.js
+++ components/SpendingList/styles.js
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
-import {WHITE, GREY} from "../../theme/colors";
-
-export const SpendingContainer = styled.View`
-	align-items: center;
-	justify-content: space-betwen;
-`;
+import { WHITE, GREY, GREEN, RED } from '../../theme/colors';
 
 export const SpendingName = styled.Text`
 	color: ${WHITE};
 	font-size: 14px;
+	margin-bottom: 2px;
 `;
-export const SpendingValue = styled.Text``;
+export const SpendingValue = styled.Text`
+	font-size: 18px;
+	margin-bottom: 2px;
+	color: ${({ sign }) => (sign === 'negative' ? RED : GREEN)};
+`;
 
 export const TimeDateStamp = styled.Text`
 	color: ${GREY};
