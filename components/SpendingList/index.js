de17d7c96608ac6f01996f6e6546d59fc0186d46
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-19 12:48:12 +0300

    Pass index property to SingleItem in SpendingList

--- components/SpendingList/index.js
+++ components/SpendingList/index.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import { View, ScrollView, Text } from 'react-native';
 import SingleItem from './SingleIem';
 
-
 export default class SpendingList extends Component {
 	render() {
 		const { items } = this.props;
 		return (
 			<View>
 				{items.map((item, i) => {
-					return <SingleItem key={i} {...item} />;
+					return <SingleItem key={i} index={i} {...item} />;
 				})}
 			</View>
 		);
