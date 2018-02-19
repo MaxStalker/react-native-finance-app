5f848889ee49a06ea7232351dd9c18745e1aeb43
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-19 12:47:13 +0300

    Add TRANSPARENT_GREY to list of colors

--- theme/colors.js
+++ theme/colors.js
@@ -1,4 +1,6 @@
 export const WHITE = "#ffffff";
+export const TRANSPARENT_GREY = "rgba(150, 150, 150, 0.25)";
+export const DARK_GREY = "#5a5a6b";
 export const GREY = "#898991";
 export const LIGHT_GREY = '#abaeb9';
 export const RED = "#ea5d64";
