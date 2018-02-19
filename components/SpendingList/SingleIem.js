c5280ae6b5997fffd96093439a116ccf27159d54
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-19 12:49:21 +0300

    Adjust alignment of elements in SpendingList

--- components/SpendingList/SingleIem.js
+++ components/SpendingList/SingleIem.js
@@ -1,24 +1,27 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
-import {SpendingName, SpendingValue, TimeDateStamp} from "./styles";
-import {Column, Row} from '../Common';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { SpendingName, SpendingValue, TimeDateStamp } from './styles';
+import { Column, Row } from '../Common';
 import TimelineChain from '../TimelineChain';
 
-export default class SingleIem extends Component{
-	render(){
-		const {name, time, date, value} = this.props;
-		return(
-			<Row justify={"space-between"}>
-				<TimelineChain/>
-				<Column>
-					<SpendingName>{name}</SpendingName>
-					<TimeDateStamp>{time}</TimeDateStamp>
-				</Column>
-				<Column>
-					<SpendingValue>{value}</SpendingValue>
-					<TimeDateStamp>{date}</TimeDateStamp>
-				</Column>
+export default class SingleIem extends Component {
+	render() {
+		const { name, time, date, value, change, index} = this.props;
+		console.log(index);
+		return (
+			<Row justify={'flex-start'}>
+				<TimelineChain number={index}/>
+				<Row justify="space-between" flex={1} padding={10} paddingRight={10}>
+					<Column align={'flex-start'}>
+						<SpendingName>{name}</SpendingName>
+						<TimeDateStamp>{time}</TimeDateStamp>
+					</Column>
+					<Column align={'flex-end'}>
+						<SpendingValue sign={change}>{value}</SpendingValue>
+						<TimeDateStamp>{date}</TimeDateStamp>
+					</Column>
+				</Row>
 			</Row>
-		)
+		);
 	}
 }
