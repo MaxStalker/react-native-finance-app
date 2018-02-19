122cea10aecaf9aef26602e2b8a48a49103cc520
Maksim Daunarovich <max.daunarovich@gmail.com>
2018-02-19 12:47:54 +0300

    Add new props for Column and Row components

--- components/Common/index.js
+++ components/Common/index.js
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 
 export const Column = styled.View`
 	flex-direction: column;
+	justify-content: ${({ justify = 'center' }) => justify};
+	align-items: ${({ align = 'center' }) => align};
+	padding: ${({ padding = '0' }) => padding +'px'};
+	paddingRight: ${({ paddingRight = '0' }) => paddingRight +'px'};
 `;
 export const Row = styled.View`
 	flex-direction: row;
-	justify-content: ${({justify = 'center'})=> justify};
-	align-items: ${({align = 'center'})=> align};
-	padding: ${({padding = '0'})=> padding+'px'};
+	justify-content: ${({ justify = 'center' }) => justify};
+	align-items: ${({ align = 'center' }) => align};
+	padding: ${({ padding = '0' }) => padding +'px'};
+	paddingRight: ${({ paddingRight = '0' }) => paddingRight +'px'};
 `;
